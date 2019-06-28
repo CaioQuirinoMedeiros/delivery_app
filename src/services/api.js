6087b60c336269f07db076c2612c749b15dc94e8
Caio Quirino Medeiros <caio.quirino.medeiros@gmail.com>
2019-06-28 03:39:11 -0300

    Setting api url in environment variable

--- src/services/api.js
+++ src/services/api.js
@@ -1,11 +1,11 @@
 /* eslint-disable import/no-cycle */
 import axios from 'axios';
-// import { REACT_APP_API_URL } from 'react-native-dotenv';
+import { REACT_APP_API_URL } from 'react-native-dotenv';
 
 import store from '../store';
 
 const api = axios.create({
-  baseURL: 'https://caio-delivery-api.herokuapp.com',
+  baseURL: REACT_APP_API_URL,
 });
 
 api.interceptors.request.use((config) => {
