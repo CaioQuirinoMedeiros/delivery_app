0e3c842613f3f702e5dd638ac56a81b23896ec93
Caio Quirino Medeiros <caio.quirino.medeiros@gmail.com>
2019-06-28 03:40:51 -0300

    Structuring main page: flatlist and click events

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
-import { View, Text, FlatList } from 'react-native';
+import {
+  View, Text, FlatList, Image,
+} from 'react-native';
 import { TouchableOpacity } from 'react-native-gesture-handler';
 
+import { REACT_APP_API_URL } from 'react-native-dotenv';
 import CategoriesActions from '../../store/ducks/categories';
 
 // import { Container } from './styles';
@@ -23,18 +26,29 @@ class Main extends Component {
     getCategoriesRequest();
   };
 
+  handleCategorySelect = (id) => {
+    const { navigation } = this.props;
+
+    navigation.navigate('Products', { categoryId: id });
+  };
+
+  renderCategory = ({ item }) => (
+    <TouchableOpacity onPress={() => this.handleCategorySelect(item.id)}>
+      <Image source={{ uri: item.image.url }} />
+      <View>
+        <Text>{item.name}</Text>
+        <Text>{item.description}</Text>
+        <Text>{item.cook_time}</Text>
+      </View>
+    </TouchableOpacity>
+  );
+
   render() {
     const { navigation } = this.props;
     const { categories } = this.props;
 
     return (
       <>
-        <View>
-          <Text>MAAAAIN</Text>
-        </View>
-        <TouchableOpacity onPress={() => navigation.navigate('Products')}>
-          <Text>NAVEEEGAR para PRODUTOS!!!!</Text>
-        </TouchableOpacity>
         <TouchableOpacity onPress={() => navigation.navigate('Cart')}>
           <Text>NAVEEEGAR para CARRINHO!!!!</Text>
         </TouchableOpacity>
@@ -53,7 +67,7 @@ class Main extends Component {
         <FlatList
           data={categories.data}
           keyExtractor={item => String(item.id)}
-          renderItem={({ item }) => <Text>{item.name}</Text>}
+          renderItem={this.renderCategory}
         />
       </>
     );
