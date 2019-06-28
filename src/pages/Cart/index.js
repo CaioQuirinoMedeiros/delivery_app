7e8a9912eb2554763d26fb4347a6ce4e973fab53
Caio Quirino Medeiros <caio.quirino.medeiros@gmail.com>
2019-06-28 07:07:57 -0300

    Structuring the Cart

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -1,25 +1,86 @@
 import React, { Component } from 'react';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+// import PropTypes from 'prop-types';
 
-import { View, Text, TouchableOpacity } from 'react-native';
-
-// import { Container } from './styles';
+import {
+  Container,
+  CartList,
+  OrderItem,
+  ProductImage,
+  OrderInfo,
+  ProductTitle,
+  ProductSize,
+  OrderItemCost,
+  ProductPrice,
+  OrderQuantity,
+  DeleteButton,
+} from './styles';
 
 class Cart extends Component {
   static navigationOptions = {
     title: 'Carrinho',
   };
 
+  static propTypes = {};
+
+  state = {
+    orderItems: [
+      {
+        id: 1,
+        title: 'Pizza calabresa',
+        size: 'Tamanho média',
+        price: 'R$19,00',
+      },
+      {
+        id: 2,
+        title: 'Pizza calabresa',
+        size: 'Tamanho média',
+        price: 'R$19,00',
+      },
+      {
+        id: 3,
+        title: 'Pizza calabresa',
+        size: 'Tamanho média',
+        price: 'R$19,00',
+      },
+      {
+        id: 4,
+        title: 'Pizza calabresa',
+        size: 'Tamanho média',
+        price: 'R$19,00',
+      },
+    ],
+  };
+
   componentDidMount() {}
 
+  renderOrderItem = ({ item }) => (
+    <OrderItem>
+      <ProductImage source={{ uri: 'sdas' }} />
+      <OrderInfo>
+        <ProductTitle>{item.title}</ProductTitle>
+        <ProductSize>{item.size}</ProductSize>
+        <OrderItemCost>
+          <ProductPrice>{item.price}</ProductPrice>
+          <OrderQuantity>{item.price}</OrderQuantity>
+        </OrderItemCost>
+      </OrderInfo>
+      <DeleteButton onPress={() => {}}>
+        <Icon name="delete-forever" size={24} />
+      </DeleteButton>
+    </OrderItem>
+  );
+
   render() {
-    const { navigation } = this.props;
+    const { orderItems } = this.state;
     return (
-      <View>
-        <Text>CARRRIIIIINHHOOOOO</Text>
-        <TouchableOpacity onPress={() => navigation.navigate('Main')}>
-          <Text>MENU</Text>
-        </TouchableOpacity>
-      </View>
+      <Container>
+        <CartList
+          data={orderItems}
+          keyExtractor={item => String(item.id)}
+          renderItem={this.renderOrderItem}
+        />
+      </Container>
     );
   }
 }
---
+++ src/pages/Cart/styles.js
@@ -0,0 +1,93 @@
+import styled from 'styled-components';
+
+export const Container = styled.View`
+  height: 100%;
+  width: 100%;
+  padding: 15px;
+
+  background: rebeccapurple;
+`;
+
+export const CartList = styled.FlatList.attrs({
+  contentContainerStyle: { paddingVertical: 5 },
+})`
+  background: yellow;
+  border-width: 2px;
+  border-color: blue;
+`;
+
+export const OrderItem = styled.TouchableOpacity.attrs({
+  activeOpacity: 0.7,
+})`
+  flex-direction: row;
+  align-items: center;
+  border-radius: 10px;
+  margin: 5px 0;
+  padding: 12px;
+
+  background: gray;
+  border-width: 1px;
+  border-color: red;
+`;
+
+export const ProductImage = styled.Image`
+  width: 80px;
+  height: 80px;
+  border-radius: 10px;
+
+  background: blue;
+  border-width: 1px;
+  border-color: black;
+`;
+
+export const OrderInfo = styled.View`
+  margin-left: 10px;
+  flex: 1;
+
+  border-width: 1px;
+  border-color: red;
+`;
+
+export const ProductTitle = styled.Text`
+  color: #0b2031;
+
+  background: green;
+`;
+
+export const ProductSize = styled.Text`
+  font-size: 13px;
+  color: #706e7b;
+  margin: 3px 0;
+
+  background: pink;
+`;
+
+export const OrderItemCost = styled.View`
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+
+  background: peru;
+`;
+
+export const ProductPrice = styled.Text`
+  color: #0b2031;
+  font-size: 15px;
+`;
+
+export const OrderQuantity = styled.Text`
+  color: #0b2031;
+  font-size: 15px;
+`;
+
+export const DeleteButton = styled.TouchableOpacity.attrs({
+  activeOpacity: 0.7,
+})`
+  height: 100%;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  padding-left: 5px;
+
+  background: red;
+`;
