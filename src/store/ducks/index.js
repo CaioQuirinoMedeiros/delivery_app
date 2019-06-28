b6765eea334fc494646e213a1288432a9376fbb6
Caio Quirino Medeiros <caio.quirino.medeiros@gmail.com>
2019-06-28 08:33:14 -0300

    Removing categories reducer

--- src/store/ducks/index.js
+++ src/store/ducks/index.js
@@ -3,11 +3,9 @@ import { combineReducers } from 'redux';
 import { toastReducer as toast } from 'react-native-redux-toast';
 import { reducer as auth } from './auth';
 import { reducer as cart } from './cart';
-import { reducer as categories } from './categories';
 
 export default () => combineReducers({
   auth,
-  categories,
   cart,
   toast,
 });
