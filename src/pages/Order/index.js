4af8a40f40aa789a17fc5c4e2698ee48cd18bfc9
Caio Quirino Medeiros <caio.quirino.medeiros@gmail.com>
2019-06-29 09:09:25 -0300

    Styled and finished Order page

--- src/pages/Order/index.js
+++ src/pages/Order/index.js
@@ -1,13 +1,36 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
 import PropTypes from 'prop-types';
-import { View, TextInput } from 'react-native';
 import axios from 'axios';
-// import { Container } from './styles';
+
+import api from '../../services/api';
+
+import CartTotal from '../../components/CartTotal';
+
+import CartActions from '../../store/ducks/cart';
+
+import {
+  Container, Input, SendOrderButton, SendOrderButtonText, InputWrapper,
+} from './styles';
 
 class Order extends Component {
   static navigationOptions = {
     title: 'Realizar pedido',
+    headerRight: <CartTotal />,
+  };
+
+  static propTypes = {
+    items: PropTypes.arrayOf(
+      PropTypes.shape({
+        id: PropTypes.number,
+        quantity: PropTypes.number,
+      }),
+    ).isRequired,
+    removeItem: PropTypes.func.isRequired,
+    navigation: PropTypes.shape({
+      navigate: PropTypes.func,
+    }).isRequired,
   };
 
   state = {
@@ -27,8 +50,38 @@ class Order extends Component {
       const { data } = await axios.get(`https://viacep.com.br/ws/${cep}/json/`);
 
       this.setState({ district: data.bairro, street: data.logradouro });
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
-      console.log(data);
+  handleOrderSubmit = async () => {
+    const {
+      notes, cep, street, district, number,
+    } = this.state;
+    const { navigation, items, removeItem } = this.props;
+
+    try {
+      await api.post('orders', {
+        observations: notes,
+        zip_code: cep,
+        district,
+        street,
+        number,
+        items: items.map(item => ({ product_size_id: item.id, quantity: item.quantity })),
+      });
+
+      this.setState({
+        notes: '',
+        cep: '',
+        street: '',
+        number: '',
+        district: '',
+      });
+
+      items.forEach(item => removeItem(item.id));
+
+      navigation.navigate('Profile');
     } catch (err) {
       console.log(err);
     }
@@ -39,71 +92,81 @@ class Order extends Component {
       notes, cep, street, district, number,
     } = this.state;
     return (
-      <View>
-        <TextInput
-          multiline
-          placeholder="Alguma observação?"
-          value={notes}
-          onChangeText={text => this.setState({ notes: text })}
-          returnKeyType="next"
-          onSubmitEditing={() => this.zipCode.focus()}
-        />
-        <TextInput
-          placeholder="Qual seu CEP?"
-          textContentType="postalCode"
-          keyboardType="numeric"
-          value={cep}
-          onChangeText={text => this.setState({ cep: text })}
-          returnKeyType="next"
-          onSubmitEditing={() => {
-            this.cepLookup();
-            this.street.focus();
-          }}
-          ref={(el) => {
-            this.zipCode = el;
-          }}
-        />
-        <TextInput
-          placeholder="Rua"
-          textContentType="streetAddressLine1"
-          value={street}
-          onChangeText={text => this.setState({ street: text })}
-          returnKeyType="next"
-          onSubmitEditing={() => this.district.focus()}
-          ref={(el) => {
-            this.street = el;
-          }}
-        />
-        <TextInput
-          placeholder="Bairro"
-          value={district}
-          onChangeText={text => this.setState({ district: text })}
-          returnKeyType="next"
-          onSubmitEditing={() => this.number.focus()}
-          ref={(el) => {
-            this.district = el;
-          }}
-        />
-        <TextInput
-          multiline
-          placeholder="Nº"
-          value={number}
-          onChangeText={text => this.setState({ number: text })}
-          returnKeyType="next"
-          ref={(el) => {
-            this.number = el;
-          }}
-        />
-      </View>
+      <Container>
+        <InputWrapper>
+          <Input
+            multiline
+            placeholder="Alguma observação?"
+            value={notes}
+            onChangeText={text => this.setState({ notes: text })}
+            returnKeyType="next"
+          />
+        </InputWrapper>
+        <InputWrapper>
+          <Input
+            placeholder="Qual seu CEP?"
+            textContentType="postalCode"
+            keyboardType="numeric"
+            value={cep}
+            onChangeText={text => this.setState({ cep: text })}
+            returnKeyType="next"
+            onSubmitEditing={() => this.street.focus()}
+            onEndEditing={() => this.cepLookup()}
+          />
+        </InputWrapper>
+        <InputWrapper>
+          <Input
+            placeholder="Rua"
+            textContentType="streetAddressLine1"
+            value={street}
+            onChangeText={text => this.setState({ street: text })}
+            returnKeyType="next"
+            onSubmitEditing={() => this.district.focus()}
+            ref={(el) => {
+              this.street = el;
+            }}
+          />
+          <Input
+            placeholder="Nº"
+            value={number}
+            keyboardType="numeric"
+            onChangeText={text => this.setState({ number: text })}
+            returnKeyType="next"
+            ref={(el) => {
+              this.number = el;
+            }}
+          />
+        </InputWrapper>
+        <InputWrapper>
+          <Input
+            placeholder="Bairro"
+            value={district}
+            onChangeText={text => this.setState({ district: text })}
+            returnKeyType="next"
+            onSubmitEditing={() => this.number.focus()}
+            ref={(el) => {
+              this.district = el;
+            }}
+          />
+        </InputWrapper>
+
+        <InputWrapper>
+          <SendOrderButton onPress={this.handleOrderSubmit}>
+            <SendOrderButtonText>FINALIZAR</SendOrderButtonText>
+          </SendOrderButton>
+        </InputWrapper>
+      </Container>
     );
   }
 }
 
 const mapStateToProps = state => ({
-  cart: state.cart,
+  items: state.cart.data,
 });
+
+const mapDispatchToProps = dispatch => bindActionCreators(CartActions, dispatch);
 
 export default connect(
   mapStateToProps,
-  null,
+  mapDispatchToProps,
 )(Order);
---
+++ src/pages/Order/styles.js
@@ -0,0 +1,52 @@
+import styled from 'styled-components';
+import { Platform } from 'react-native';
+
+export const Container = styled.KeyboardAvoidingView.attrs({
+  behavior: Platform.OS === 'iOS' ? 'padding' : '',
+})`
+  width: 100%;
+  flex: 1;
+  align-items: center;
+`;
+
+export const InputWrapper = styled.View`
+  width: 90%;
+  flex-direction: row;
+  justify-content: flex-end;
+`;
+
+export const Input = styled.TextInput`
+  flex: ${props => (props.placeholder === 'Rua' ? 5 : 1)};
+  height: ${props => (props.multiline ? '100px' : 'auto')};
+  border-radius: 10px;
+  padding: 15px;
+  margin: 5px 0;
+  margin-right: ${props => (props.placeholder === 'Rua' ? '5px' : 0)};
+  background: #fff;
+
+  shadow-opacity: 0.7;
+  shadow-radius: 11px;
+  shadow-color: #000;
+  shadow-offset: 10px 10px;
+  elevation: 8;
+`;
+
+export const SendOrderButton = styled.TouchableOpacity.attrs({
+  activeOpacity: 0.7,
+})`
+  align-self: flex-end;
+  height: 40px;
+  margin-top: 20px;
+  padding: 0 30px;
+  border-radius: 20px;
+  background: #e62638;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const SendOrderButtonText = styled.Text`
+  font-size: 16px;
+  font-weight: bold;
+  color: #fff;
+  text-align: center;
+`;
