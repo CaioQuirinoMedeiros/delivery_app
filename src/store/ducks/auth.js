fa12771c0ebbf6ab5eb51a52450fb22160fbe42c
Caio Quirino Medeiros <caio.quirino.medeiros@gmail.com>
2019-07-05 17:09:37 -0300

    Removing loading, it was causing a bug

--- src/store/ducks/auth.js
+++ src/store/ducks/auth.js
@@ -9,8 +9,7 @@ const { Types, Creators } = createActions({
   signInSuccess: ['token'],
   signInFailure: [],
   signOut: null,
-  signUpRequest: ['name', 'email', 'password'],
-  signUpSuccess: null,
+  signUpRequest: ['name', 'email', 'password', 'password_confirmation'],
   initCheckSuccess: null,
 });
 
@@ -25,17 +24,12 @@ export const INITIAL_STATE = Immutable({
   signedIn: false,
   token: null,
   roles: [],
-  loading: false,
 });
 
 /**
  * Reducers
  */
-const request = state => state.merge({ loading: true });
-
-const success = (state, { token }) => state.merge({ signedIn: true, token, loading: false });
-
-const failure = state => state.merge({ loading: false });
+const success = (state, { token }) => state.merge({ signedIn: true, token });
 
 const logout = state => state.merge({ signedIn: false, token: null });
 
@@ -45,9 +39,7 @@ const checkSuccess = state => state.merge({ authChecked: true });
  * Reducers to types
  */
 export const reducer = createReducer(INITIAL_STATE, {
-  [Types.SIGN_IN_REQUEST]: request,
   [Types.SIGN_IN_SUCCESS]: success,
-  [Types.SIGN_IN_FAILURE]: failure,
   [Types.SIGN_OUT]: logout,
   [Types.INIT_CHECK_SUCCESS]: checkSuccess,
 });
